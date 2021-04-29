d891aa80794699332b4a99628cf606f3749fb559
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-04-29 17:47:48 -0400

    created login screen and implemented user prompts

---
+++ src/com/revature/quizzard/screens/LoginScreen.java
@@ -0,0 +1,30 @@
+package com.revature.quizzard.screens;
+
+public class LoginScreen {
+
+    private BufferedReader consoleReader;
+
+    public LoginScreen(BufferedReader consoleReader) {
+        this.consoleReader = consoleReader;
+    }
+
+    public void render() {
+
+        String username;
+        String password;
+
+        try {
+            System.out.println("Login to your account!");
+            System.out.println("+-------------------------+");
+
+            System.out.print("Username: ");
+            username = consoleReader.readLine(); // throws Exception here
+
+            System.out.print("Password: ");
+            password = consoleReader.readLine();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ sudoCode.md
@@ -0,0 +1,6 @@
+1. User input
+    - getting username and password
+        - modify that existing code
+        - keep them strings
+2. Check file for login
+    - 
