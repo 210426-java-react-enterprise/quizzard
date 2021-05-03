35b5810e6cd79d5c52702ab0e5c90462d39d43fa
jbialon32 <jbialon32@gmail.com>
2021-05-03 09:43:26 -0500

    pushing to pull

---
+++ src/com/revature/quizzard/util/List.java
@@ -0,0 +1,9 @@
+package com.revature.quizzard.util;/**
+ * Created by IntelliJ IDEA.
+ * User: Jbialon
+ * Date: 4/30/2021
+ * Time: 4:05 PM
+ * Description: {Insert Description}
+ */
+ public interface List {
+}
