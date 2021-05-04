b348f526055c55837fb6cb37d484cf63e99a2182
Wezley <wezley.singleton@gmail.com>
2021-05-04 17:46:13 -0400

    push for EJ

--- src/main/java/com/revature/quizzard/util/ConnectionFactory.java
+++ src/main/java/com/revature/quizzard/util/ConnectionFactory.java
@@ -1,15 +1,31 @@
 package com.revature.quizzard.util;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionFactory {
 
     private static ConnectionFactory connectionFactory;
+    private Properties props = new Properties();
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
     private ConnectionFactory() {
-
+        try {
+            props.load(new FileReader("src/main/resources/application.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static ConnectionFactory getInstance() {
@@ -25,10 +41,12 @@ public class ConnectionFactory {
         Connection conn = null;
 
         try {
-
-            conn = DriverManager.getConnection("host-url", "username", "password");
+            conn = DriverManager.getConnection(
+                    props.getProperty("host-url"),
+                    props.getProperty("username"),
+                    props.getProperty("password"));
         } catch (SQLException e) {
-
+            e.printStackTrace();
         }
 
         return conn;
