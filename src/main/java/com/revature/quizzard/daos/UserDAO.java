6dc37621dc8b652fa9b4039def8d1ab3449f02cf
EJ <eobarobe@uvm.edu>
2021-05-09 14:56:00 -0400

    commiting for good luck

--- src/main/java/com/revature/quizzard/daos/UserDAO.java
+++ src/main/java/com/revature/quizzard/daos/UserDAO.java
@@ -2,8 +2,6 @@ package com.revature.quizzard.daos;
 
 import com.revature.quizzard.models.AppUser;
 import com.revature.quizzard.util.ConnectionFactory;
-
-import java.io.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -13,14 +11,21 @@ public class UserDAO {
    public void save(AppUser newUser){
        try(Connection conn = ConnectionFactory.getInstance().getConnection()) {
            String sqlInsertUser = "insert into quizzard.users (username , password , email , first_name , last_name , age ) values (?,?,?,?,?,?)";
-           PreparedStatement pstmt = conn.prepareStatement((sqlInsertUser));
+           PreparedStatement pstmt = conn.prepareStatement(sqlInsertUser,new String[]{"user_id"});
            pstmt.setString(1,newUser.getUsername());
            pstmt.setString(2,newUser.getPassword());
            pstmt.setString(3,newUser.getEmail());
            pstmt.setString(4,newUser.getFirstName());
            pstmt.setString(5,newUser.getLastName());
            pstmt.setInt(6,newUser.getAge());
-           pstmt.executeUpdate();
+           int rowsInserted = pstmt.executeUpdate();
+           //grabs the generated key from the new user saved
+           if(rowsInserted != 0){
+               ResultSet rs = pstmt.getGeneratedKeys();
+               while(rs.next()){
+                   newUser.setId(rs.getInt("user_id"));
+               }
+           }
        } catch (SQLException throwables) {
            throwables.printStackTrace();
        }
@@ -44,15 +49,58 @@ public class UserDAO {
                 user.setFirstName(rs.getString("first_name"));
                 user.setLastName(rs.getString("last_name"));
                 user.setAge(rs.getInt("age"));
+
             }
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
     }
 
     // TODO implement me: You can only delete an account when signed in
-    public void deleteAccount(){
+    public void deleteAccount(AppUser user){
+        try(Connection conn = ConnectionFactory.getInstance().getConnection()){
+            String sql = "delete * from quizzard.users where user_id = ?";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1,user.getId());
 
+            ResultSet rs = pstmt.executeQuery();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+    public Boolean isEmailAvailible(String email){
+       try(Connection conn = ConnectionFactory.getInstance().getConnection()){
+            String sql = "select * from quizzard.users where email = ?";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1,email);
+            ResultSet rs = pstmt.executeQuery();
+
+            if(rs.next()){
+                return false;
+            }
+        } catch (SQLException throwables) {
+           throwables.printStackTrace();
+        }
+       return true;
+    }
+    public Boolean isUsernameAvailible(String username){
+        try(Connection conn = ConnectionFactory.getInstance().getConnection()){
+            String sql = "select * from quizzard.users where username = ?";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1,username);
+            ResultSet rs = pstmt.executeQuery();
+
+            if(rs.next()){
+                return false;
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return true;
     }
 }
--- src/main/java/com/revature/quizzard/services/UserService.java
+++ src/main/java/com/revature/quizzard/services/UserService.java
@@ -1,4 +1,37 @@
 package com.revature.quizzard.services;
+import com.revature.quizzard.daos.UserDAO;
+import com.revature.quizzard.models.AppUser;
+import com.revature.quizzard.exceptions.ResourcePersistenceException;
+import com.revature.quizzard.exceptions.InvalidRequestException;
 
 public class UserService {
+    private UserDAO userDao;
+
+    public UserService(UserDAO userDao){
+        this.userDao = userDao;
+    }
+    public void register(AppUser newUser){
+        //valid dates entries upon registration using our own custom exceptions
+        if (!isUserValid(newUser)){
+            throw new InvalidRequestException("Invalid new user data provided! ");
+        }
+        if (!userDao.isUsernameAvailible(newUser.getUsername())){
+            throw new ResourcePersistenceException("Username taken");
+        }
+        if (!userDao.isEmailAvailible(newUser.getUsername())){
+            throw new ResourcePersistenceException("Email has been used already");
+        }
+        userDao.save(newUser);
+    }
+
+    public boolean isUserValid(AppUser user){
+        if (user == null) return false;
+        if (user.getUsername()==null|| user.getUsername().trim().isEmpty() || user.getUsername().length() > 20 )  return false;
+        if (user.getPassword()==null|| user.getPassword().trim().isEmpty() || user.getPassword().length() > 255 ) return false;
+        if (user.getEmail()==null|| user.getEmail().trim().isEmpty() || user.getEmail().length() > 255 ) return false;
+        if (user.getFirstName()==null|| user.getFirstName().trim().isEmpty() || user.getFirstName().length() > 25 ) return false;
+        if (user.getLastName()==null|| user.getLastName().trim().isEmpty() || user.getLastName().length() > 25 ) return false;
+        if (user.getAge()<0 )  return false;
+     return true;
+    }
 }
