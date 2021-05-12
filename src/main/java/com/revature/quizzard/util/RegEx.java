1b14f77385403e764508309241c7690a15253faf
Wezley <wezley.singleton@gmail.com>
2021-05-12 13:27:14 -0400

    added regex to validate user's dashboard screen menu selection

--- src/main/java/com/revature/quizzard/util/RegEx.java
+++ src/main/java/com/revature/quizzard/util/RegEx.java
@@ -37,6 +37,11 @@ public enum RegEx {
     VALID_WELCOME_SCREEN_INPUT("[1-3]"),
 
     /**
+     * Only numeric values 1, 2, 3, and 4 allowed.
+     */
+    VALID_DASHBOARD_SCREEN_INPUT("[1-4]"),
+
+    /**
      * A non-value used as a default when no pattern validation is necessary.
      */
     NONE("");
